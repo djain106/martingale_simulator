30774f2ba45ba9845ff6b45a53b116fe643ecafe
Daksh Jain <dakshbjain@gmail.com>
2021-02-06 18:26:56 -0500

    added more stuff

--- martingale_simulation.py
+++ martingale_simulation.py
@@ -1,36 +1,54 @@
-import os
 import random
 import time
+import matplotlib.pyplot as plt
 
 # Create limbo game with betting to simulate losses/gains using martingale strategy
 seed = round(time.time()*1000)
 random.seed(seed)
 
-odds = 0.505
+loss_odds = 0.505
 rounds = int(input("Number of rounds: "))
-balance = 100
+balance = 600
+initial_balance = balance
 initial_bet = 0.02
 bet = initial_bet
 max_bet = initial_bet
 reserve = 0
+reserve_limit = 100
+loss = 0
+stop_loss = 80
+round_nums = []
+balances = []
+last_win = 0
 
-for r in range(rounds):
+for r in range(1, rounds+1):
     multiplier = random.random()
-    if balance > 200:
-        reserve += 100
-        balance -= 100
-    if balance < bet:
-        print("Round: ", r)
+    round_nums.append(r)
+    balances.append(balance - initial_balance)
+    if balance > initial_balance + reserve_limit:
+        reserve += reserve_limit
+        balance -= reserve_limit
+    if loss > stop_loss:
+        print("Loss: ", loss)
+        print("Final Round: ", r)
         break
+    print("Win, " + str(r - last_win - 1)
+          if multiplier > loss_odds else "Lose")
     balance -= bet
-    if multiplier > odds:
+    if multiplier > loss_odds:
         balance += 2*bet
         bet = initial_bet
+        loss = 0
+        last_win = r
     else:
+        loss += bet
         bet *= 2
     max_bet = max(bet, max_bet)
-    print(("Win" if multiplier > odds else "Lose") +
-          ": ", round(multiplier, 2), bet)
 
-print(round(balance, 2))
-print("Reserve: ", reserve)
+print("Final Balance:", balance)
+print("Reserve:", reserve)
+plt.plot(round_nums, balances)
+plt.xlabel('Round')
+plt.ylabel('Profit')
+plt.title('Profit Plot')
+plt.show()
